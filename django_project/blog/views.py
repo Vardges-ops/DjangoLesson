159198aaa62a194a2eab8cc34bb697842c5401f2
Vardges <sukiasyan.vardges@gmail.com>
2023-02-10 23:36:59 +0400

    Added post deletion
    Created post delete class view,
    Created post delete template,
    Changed all templated related to post delete in blog app

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,7 +3,8 @@ from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
@@ -43,6 +44,18 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
 
     def test_func(self):
         post = self.get_object()
